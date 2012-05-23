cc6434070990bc5cfad7777147010db5d5762425
ospalh <ospalh@gmail.com>
2012-05-23 16:11:45 +0200

    Added a Chinese model It’s just a variant of the Japaneses model from the Japanese add-on. Needs work. Especially the fonts.

--- chinese/model.py
+++ chinese/model.py
@@ -1,3 +1,43 @@
-# -*- coding: utf-8 ; mode: python -*-
-# © 2012: Roland Sieker <ospalh@gmail.com>
+# -*- coding: utf-8 -*-
+# 
+# Copyright © 2012 Roland Sieker <ospalh@gmail.com>
+# Original: Damien Elmes <anki@ichi2.net> (as japanese/model.py)
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+#
+# Standard Chinese model.
+#
+
+import anki.stdmodels
+
+def addChineseModel(col):
+    mm = col.models
+    m = mm.new(_("Chinese"))
+    fm = mm.newField(_("Hanzi"))
+    mm.addField(m, fm)
+    fm = mm.newField(_("Meaning"))
+    mm.addField(m, fm)
+    fm = mm.newField(_("Pinyin"))
+    mm.addField(m, fm)
+    t = mm.newTemplate(_("Recognition"))
+    t['qfmt'] = "<div class=chinese>{{Hanzi}}</div>"
+    t['afmt'] = """{{FrontSide}}<hr id=answer>\
+<div class=chinese>{{ruby:Ruby}}</div>\
+<div>{{Meaning}}</div>"""
+    mm.addTemplate(m, t)
+    t = mm.newTemplate(_("Recall"))
+    t['qfmt'] = "<div>{{Meaning}}</div>"
+    t['afmt'] = """{{FrontSide}}<hr id=answer>\
+<div class=chinese>{{ruby:Ruby}}</div>"""
+    mm.addTemplate(m, t)
+    # css
+    m['css'] += u"""\
+.chinese { font-size: 30px }
+.win .chinese { font-family: "MS Mincho", "ＭＳ 明朝"; }
+.mac .chinese { font-family: "Hiragino Mincho Pro", "ヒラギノ明朝 Pro"; }
+.linux .chinese { font-family: "Kochi Mincho", "東風明朝"; }
+.mobile .chinese { font-family: "Hiragino Mincho ProN"; }"""
+    # recognition card
+    mm.add(m)
+    return m
+
+anki.stdmodels.models.append((_("Chinese"), addChineseModel))
