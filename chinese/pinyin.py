f7f72eb27b3c091aca0d5cc2a855dce49b2e560a
ospalh <ospalh@gmail.com>
2012-06-13 14:30:24 +0200

    Seems to be almost usable.

--- chinese/pinyin.py
+++ chinese/pinyin.py
@@ -2,17 +2,29 @@
 #
 # Copyricht © 2012 Roland Sieker, <ospalh@gmail.com>
 # 
-# Large portions of this file were originally written by
+# Portions of this file were originally written by
 # Damien Elmes <anki@ichi2.net>
-# and
+# as part of the Japanese addon.
 #
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 #
+# The data in the two .json files are derived from Unihan.zip data:
+#COPYRIGHT AND PERMISSION NOTICE
+
+#Copyright © 1991-2012 Unicode, Inc. All rights reserved. Distributed under the Terms of Use in http://www.unicode.org/copyright.html.
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy of the Unicode data files and any associated documentation (the "Data Files") or Unicode software and any associated documentation (the "Software") to deal in the Data Files or Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, and/or sell copies of the Data Files or Software, and to permit persons to whom the Data Files or Software are furnished to do so, provided that (a) the above copyright notice(s) and this permission notice appear with all copies of the Data Files or Software, (b) both the above copyright notice(s) and this permission notice appear in associated documentation, and (c) there is clear notice in each modified Data File or in the Software as well as in the documentation associated with the Data File(s) or Software that the data or software has been modified.
+
+#THE DATA FILES AND SOFTWARE ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THE DATA FILES OR SOFTWARE.
+
+#Except as contained in this notice, the name of a copyright holder shall not be used in advertising or otherwise to promote the sale, use or other dealings in these Data Files or Software without prior written authorization of the copyright holder.
+
 
 import sys
 import os
-from anki.utils import stripHTML
-from anki.db import *
+import json
+from aqt import mw
+
 
 
 hanzi_fields = ['Chinese', 'Mandarin', 'Hanzi', u'汉字', u'漢字', 'Expression']
@@ -20,14 +32,34 @@ ruby_fields = ['Reading', 'Ruby', 'Pinyin']
 tones_fields = ['Tones']
 
 
+
 model_name = 'chinese'
 
-#type = "cantonese"
-type = "mandarin"
+type = "cantonese"
+#type = "mandarin"
 
 tone_classes =  {
-    1 : 'tone1', 2 : 'tone2', 3 : 'tone3', 4 : 'tone4', 5 : 'tone5'
+    1 : 'tone1', 2 : 'tone2', 3 : 'tone3', 4 : 'tone4', 5 : 'tone5',
+    # You will have to define your own colors and meanings for the
+    # extra 'tones'.
+    6: 'tone6', 7: 'tone7',    8: 'tone8', 9: 'tone9'
     }
+
+
+# To get the tone numbers from mandarin readings. Values taken from
+# wikipedia. Not in this dict is taken as tone 5.
+vowel_tone_dict = {
+    u'ā':1, u'ā':1, u'ɑ̄':1, u'ē':1, u'ī':1, u'ō':1, u'ū':1,
+    u'ǖ':1, u'Ā':1, u'Ē':1, u'Ī':1, u'Ō':1, u'Ū':1, u'Ǖ':1,
+    u'á':2, u'ɑ́':2, u'é':2, u'í':2, u'ó':2, u'ú':2, u'ǘ':2,
+    u'Á':2, u'É':2, u'Í':2, u'Ó':2, u'Ú':2, u'Ǘ':2,
+    u'ǎ':3, u'ɑ̌':3, u'ě':3, u'ǐ':3, u'ǒ':3, u'ǔ':3, u'ǚ':3,
+    u'Ǎ':3, u'Ě':3, u'Ǐ':3, u'Ǒ':3, u'Ǔ':3, u'Ǚ':3,
+    u'à':4, u'ɑ̀':4, u'è':4, u'ì':4, u'ò':4, u'ù':4, u'ǜ':4,
+    u'À':4, u'È':4, u'Ì':4, u'Ò':4, u'Ù':4, u'Ǜ':4
+    }
+
+
 
 
 
@@ -42,41 +74,22 @@ def is_han_character(uchar):
     return False
 
 
-class UnihanController(object):
-    """Class to get pinyin for a character.
-
-    These are some remnants of the unihan reading extractor from
-    Damien Elmes' chinese plugin.
-    """
-    
-    def __init__(self, target):
-        if sys.platform.startswith("win32"):
-           base = unicode(os.path.dirname(os.path.abspath(__file__)),
-                          "mbcs")
-        else:
-           base = os.path.dirname(os.path.abspath(__file__))
-        self.db = DB(os.path.abspath(os.path.join(base, "unihan.db")))
-        self.type = target
-
-    def reading(self, hanzi):
-        """Get pinyin for a single character"""
-        n = ord(hanzi)
-        return self.db.execute("select %s from unihan where id = :id"
-                               % self.type, id=n)
-
 
 class Pinyinizer(object):
     
     def __init__(self):
-        self.unihan = UnihanController(type)
-
+        # N.B. This assumes that type is either 'cantonese' or
+        # 'mandarin'.
+        readings_file_name = os.path.join(mw.pm.addonFolder(), 'chinese',
+                                          type + '_readings.json')
+        self.readings = json.load(open(readings_file_name))
+            
 
     def get_pinyin_data(self, hanzi):
-        # There are fifty ways to^H^H for this to blow up.
-
-        pinyin = self.unihan.reading(hanzi)
-        for p in pinyin:
-            print 'pinyin: ', p
+        # This may throw a KeyError
+        pinyin = self.readings[hanzi]
+        # Ignore all put the first reading
+        pinyin = pinyin.split(' ')[0]
         tone_number = self._tone_number(pinyin)
         # Carfully craft the ruby format string so the build-in
         # furigana &c. templates can parse this and the colorization
@@ -90,8 +103,17 @@ class Pinyinizer(object):
         return pinyin, ruby, tone_number
 
     def _tone_number(self, pinyin):
+        if type == 'cantonese':
         # Another fifty ways to blow up.
-        return int(pinyin[-1:])
+            return int(pinyin[-1:])
+        else:
+            for c in pinyin:
+                try:
+                    return vowel_tone_dict[c]
+                except KeyError:
+                    continue
+            return 5
+                
 
 
 pinyinize = Pinyinizer()
