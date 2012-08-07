c561d455153b600c48a253105e8c4e4e789894d6
ospalh <ospalh@gmail.com>
2012-08-07 12:14:09 +0200

    ce

--- chinese/pinyin.py
+++ chinese/pinyin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyricht © 2012 Roland Sieker, <ospalh@gmail.com>
-# 
+#
 # Portions of this file were originally written by
 # Damien Elmes <anki@ichi2.net>
 # as part of the Japanese addon.
@@ -9,15 +9,42 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 #
 # The data in the two .json files are derived from Unihan.zip data:
-#COPYRIGHT AND PERMISSION NOTICE
+# COPYRIGHT AND PERMISSION NOTICE
 
-#Copyright © 1991-2012 Unicode, Inc. All rights reserved. Distributed under the Terms of Use in http://www.unicode.org/copyright.html.
+# Copyright © 1991-2012 Unicode, Inc. All rights reserved. Distributed
+# under the Terms of Use in http://www.unicode.org/copyright.html.
 
-#Permission is hereby granted, free of charge, to any person obtaining a copy of the Unicode data files and any associated documentation (the "Data Files") or Unicode software and any associated documentation (the "Software") to deal in the Data Files or Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, and/or sell copies of the Data Files or Software, and to permit persons to whom the Data Files or Software are furnished to do so, provided that (a) the above copyright notice(s) and this permission notice appear with all copies of the Data Files or Software, (b) both the above copyright notice(s) and this permission notice appear in associated documentation, and (c) there is clear notice in each modified Data File or in the Software as well as in the documentation associated with the Data File(s) or Software that the data or software has been modified.
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of the Unicode data files and any associated
+# documentation (the "Data Files") or Unicode software and any
+# associated documentation (the "Software") to deal in the Data Files
+# or Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, and/or sell
+# copies of the Data Files or Software, and to permit persons to whom
+# the Data Files or Software are furnished to do so, provided that (a)
+# the above copyright notice(s) and this permission notice appear with
+# all copies of the Data Files or Software, (b) both the above
+# copyright notice(s) and this permission notice appear in associated
+# documentation, and (c) there is clear notice in each modified Data
+# File or in the Software as well as in the documentation associated
+# with the Data File(s) or Software that the data or software has been
+# modified.
 
-#THE DATA FILES AND SOFTWARE ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THE DATA FILES OR SOFTWARE.
+# THE DATA FILES AND SOFTWARE ARE PROVIDED "AS IS", WITHOUT WARRANTY
+# OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR
+# ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY
+# DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+# OF THE DATA FILES OR SOFTWARE.
 
-#Except as contained in this notice, the name of a copyright holder shall not be used in advertising or otherwise to promote the sale, use or other dealings in these Data Files or Software without prior written authorization of the copyright holder.
+# Except as contained in this notice, the name of a copyright holder
+# shall not be used in advertising or otherwise to promote the sale,
+# use or other dealings in these Data Files or Software without prior
+# written authorization of the copyright holder.
 
 
 import sys
@@ -76,14 +103,14 @@ def is_han_character(uchar):
 
 
 class Pinyinizer(object):
-    
+
     def __init__(self):
         # N.B. This assumes that type is either 'cantonese' or
         # 'mandarin'.
         readings_file_name = os.path.join(mw.pm.addonFolder(), 'chinese',
                                           type + '_readings.json')
         self.readings = json.load(open(readings_file_name))
-            
+
 
     def get_pinyin_data(self, hanzi):
         # This may throw a KeyError
@@ -103,6 +130,13 @@ class Pinyinizer(object):
         return pinyin, ruby, tone_number
 
     def _tone_number(self, pinyin):
+        """Return a tone number.
+
+        Return a tone number, depending on the type, either derived
+        from the end of the pinyin string (cantonese) or determined
+        depending on the first "decorated" vowel we find (mandarin).
+
+        """
         if type == 'cantonese':
         # Another fifty ways to blow up.
             return int(pinyin[-1:])
@@ -113,7 +147,7 @@ class Pinyinizer(object):
                 except KeyError:
                     continue
             return 5
-                
+
 
 
 pinyinize = Pinyinizer()
@@ -175,6 +209,3 @@ def on_focus_lost(flag, n, fidx):
         # But clobber what is alread there
         n[tones_field] = tones
     return True
-
-
-
