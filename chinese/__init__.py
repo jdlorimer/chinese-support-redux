82161458f7f7efdd486af1139b45cbcdf322e2ea
Thomas (Johnix) TEMPE <thomas.tempe@alysse.org>
2013-08-02 10:53:45 +0800

    Downgrade back to 0.7.5, as 0.7.6 caused an issue.

--- chinese/__init__.py
+++ chinese/__init__.py
@@ -9,5 +9,5 @@
 #This should stay written exactly this way, and keep the same parenthesis
 #format, as it is parsed by a very simple regexp to check for next
 #release info from github, in config.py
-__version__ = """0.7.6"""
-release_info = """New "fill missing sound" feature; first-run bug fix on Windows."""
+__version__ = """0.7.7"""
+release_info = """Same as 0.7.5, fixes the bug with 0.7.6 on Windows."""
