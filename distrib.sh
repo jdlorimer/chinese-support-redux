dd82175a6d7cb2363e0f8af224496ce8372e6877
Thomas (Johnix) TEMPE <thomas.tempe@alysse.org>
2014-10-06 00:19:49 +0800

    Rebuild the dictionary DB and strip the index, so as to save space

--- distrib.sh
+++ distrib.sh
@@ -3,8 +3,7 @@ mkdir distrib
 rm -rf distrib/*
 cp -ar Chinese_support.py chinese distrib
 cd distrib
-find -name \*~ |xargs rm
-find -name \*.pyc |xargs rm
+
 cd chinese
 rm chinese_addon_config.json
 rm edit_behavior.py
@@ -13,4 +12,10 @@ cd db
 ./update --cleanup
 rm -rf tmp
 cd ../..
+
+find -name \*~ |xargs rm
+find -name \*.pyc |xargs rm
+find -name \*.pyo |xargs rm
+find -name .DS_Store |xargs rm
+
 zip -r chinese_support Chinese_support.py chinese --exclude \*~ \#* \*.orig Logo .DS_Store
