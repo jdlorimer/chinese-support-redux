0761a54f73081ac25292b993f112d77b81b2e40a
Thomas (Johnix) TEMPE <thomas.tempe@alysse.org>
2014-10-07 15:58:28 +0800

    New version 9.2

--- chinese/__init__.py
+++ chinese/__init__.py
@@ -9,11 +9,13 @@
 #This should stay written exactly this way, and keep the same parenthesis
 #format, as it is parsed by a very simple regexp to check for next
 #release info from github, in config.py
-__version__ = """0.9.1"""
+__version__ = """0.9.2"""
 ankiweb_number = "3448800906"
-release_info = """<ul>
-<li>Improved "Fill missing sounds" function.</li>
-<li>Local dictionaries now point out words with multiple pronunciations.</li>
-<li>Now offering two default models, "basic" and "advanced", to better accomodate new users.</li>
+release_info = """This version brings:<ul>
+<li>Improved Bopomofo transcription, thanks to <b>Alex Griffin</b>.</li>
+<li>Multiple bugfixes thanks to <b>Joel Kitching</b>.</li>
+<li><b>Added speech support from Baidu Translate</b> (which is not censored in PR China), courtesy of <b>Hoeken</b>.</li>
+<li>Improved help messages and updated documentation.</li>
+<li>New <a href="https://anki.tenderapp.com/discussions/add-ons/1646-chinese-support-add-on">Support forum</a>.</li>
 </ul>
 """
