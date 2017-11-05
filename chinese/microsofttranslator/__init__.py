d0bffa9405cfd923440b257a599affc56d5ba08c
luoliyan <joseph.lorimer@posteo.net>
2017-11-05 19:07:49 +0930

    Update Microsoft Translator

--- chinese/microsofttranslator/__init__.py
+++ chinese/microsofttranslator/__init__.py
@@ -6,24 +6,19 @@
 
     http://msdn.microsoft.com/en-us/library/ff512419.aspx
 
-    :copyright: © 2011 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
 """
 
 __all__ = ['Translator', 'TranslateApiException']
 
 try:
     import simplejson as json
-    from simplejson import JSONDecodeError
 except ImportError:
     import json
-    class JSONDecodeError(Exception): pass
-    # Ugly: No alternative because this exception class doesnt seem to be there
-    # in the standard python module
-import urllib
-import urllib2
-import warnings
+
+from . import six
 import logging
+import requests
+import warnings
 
 
 class ArgumentOutOfRangeException(Exception):
@@ -44,7 +39,10 @@ class Translator(object):
     :param app_id: A string containing the Bing AppID. (Deprecated)
     """
 
-    def __init__(self, client_id, client_secret,
+    base_url = "http://api.microsofttranslator.com/V2/Ajax.svc"
+
+    def __init__(
+            self, client_id, client_secret,
             scope="http://api.microsofttranslator.com",
             grant_type="client_credentials", app_id=None, debug=False):
         """
@@ -96,15 +94,16 @@ class Translator(object):
 
         :return: The access token to be used with subsequent requests
         """
-        args = urllib.urlencode({
+        args = {
             'client_id': self.client_id,
             'client_secret': self.client_secret,
             'scope': self.scope,
             'grant_type': self.grant_type
-        })
-        response = json.loads(urllib.urlopen(
-            'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13', args
-        ).read())
+        }
+        response = requests.post(
+            'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13',
+            data=args
+        ).json()
 
         self.logger.debug(response)
 
@@ -115,30 +114,40 @@ class Translator(object):
             )
         return response['access_token']
 
-    def call(self, url, params):
-        """Calls the given url with the params urlencoded
+    def call(self, path, params):
+        """Calls the given path with the params urlencoded
+
+        :param path: The path of the API call being made
+        :param params: The parameters dictionary
         """
         if not self.access_token:
             self.access_token = self.get_access_token()
 
-        request = urllib2.Request(
-            "%s?%s" % (url, urllib.urlencode(params)),
+        resp = requests.get(
+            "/".join([self.base_url, path]),
+            params=params,
             headers={'Authorization': 'Bearer %s' % self.access_token}
         )
-        response = urllib2.urlopen(request).read()
-        rv =  json.loads(response.decode("UTF-8-sig"))
+        resp.encoding = 'UTF-8-sig'
+        rv = resp.json()
 
-        if isinstance(rv, basestring) and \
+        if isinstance(rv, six.string_types) and \
                 rv.startswith("ArgumentOutOfRangeException"):
             raise ArgumentOutOfRangeException(rv)
 
-        if isinstance(rv, basestring) and \
+        if isinstance(rv, six.string_types) and \
                 rv.startswith("TranslateApiException"):
             raise TranslateApiException(rv)
 
+        if isinstance(rv, six.string_types) and \
+                rv.startswith(("ArgumentException: "
+                               "The incoming token has expired")):
+            self.access_token = None
+            return self.call(path, params)
         return rv
 
-    def translate(self, text, to_lang, from_lang=None,
+    def translate(
+            self, text, to_lang, from_lang=None,
             content_type='text/plain', category='general'):
         """Translates a text string from one language to another.
 
@@ -159,34 +168,32 @@ class Translator(object):
             'to': to_lang,
             'contentType': content_type,
             'category': category,
-            }
+        }
         if from_lang is not None:
             params['from'] = from_lang
-        return self.call(
-            "http://api.microsofttranslator.com/V2/Ajax.svc/Translate",
-            params)
+        return self.call("Translate", params)
 
     def translate_array(self, texts, to_lang, from_lang=None, **options):
         """Translates an array of text strings from one language to another.
 
         :param texts: A list containing texts for translation.
-        :param to_lang: A string representing the language code to 
+        :param to_lang: A string representing the language code to
             translate the text into.
-        :param from_lang: A string representing the language code of the 
-            translation text. If left None the response will include the 
+        :param from_lang: A string representing the language code of the
+            translation text. If left None the response will include the
             result of language auto-detection. (Default: None)
-        :param options: A TranslateOptions element containing the values below. 
+        :param options: A TranslateOptions element containing the values below.
             They are all optional and default to the most common settings.
 
-                Category: A string containing the category (domain) of the 
+                Category: A string containing the category (domain) of the
                     translation. Defaults to "general".
-                ContentType: The format of the text being translated. The 
-                    supported formats are "text/plain" and "text/html". Any 
+                ContentType: The format of the text being translated. The
+                    supported formats are "text/plain" and "text/html". Any
                     HTML needs to be well-formed.
-                Uri: A string containing the content location of this 
+                Uri: A string containing the content location of this
                     translation.
                 User: A string used to track the originator of the submission.
-                State: User state to help correlate request and response. The 
+                State: User state to help correlate request and response. The
                     same contents will be returned in the response.
         """
         options = {
@@ -195,15 +202,28 @@ class Translator(object):
             'Uri': '',
             'User': 'default',
             'State': ''
-            }.update(options)
+        }.update(options)
         params = {
             'texts': json.dumps(texts),
             'to': to_lang,
             'options': json.dumps(options),
-            }
+        }
         if from_lang is not None:
             params['from'] = from_lang
 
-        return self.call(
-                "http://api.microsofttranslator.com/V2/Ajax.svc/TranslateArray",
-                params)
+        return self.call("TranslateArray", params)
+
+    def get_languages(self):
+        """Fetches the languages supported by Microsoft Translator
+           Returns list of languages
+        """
+        return self.call('GetLanguagesForTranslate', '')
+
+    def detect_language(self, text):
+        """Detects language of given string
+           Returns two letter language - Example : fr
+        """
+        params = {
+            'text': text.encode('utf8')
+        }
+        return self.call('Detect', params)
