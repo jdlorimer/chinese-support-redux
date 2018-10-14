1cfb326e39d7b4990127d46b1a4d63f138800092
luoliyan <joseph.lorimer@posteo.net>
2018-10-14 17:58:35 +0930

    Fix paragraph handling

--- convert-readme.py
+++ convert-readme.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from re import MULTILINE, sub
+from re import sub
 
 from markdown2 import markdown
 
 
 def main():
-    '''Covert GitHub mardown to AnkiWeb HTML.'''
+    """Covert GitHub mardown to AnkiWeb HTML."""
     # permitted tags: img, a, b, i, code, ul, ol, li
 
     translate = [
@@ -16,6 +16,7 @@ def main():
         (r'<strong>([^<]+)</strong>', r'<b>\1</b>'),
         (r'<em>([^<]+)</em>', r'<i>\1</i>'),
         (r'<kbd>([^<]+)</kbd>', r'<code><b>\1</b></code>'),
+        (r'</a></p>', r'</a></p>\n'),
         (r'<p>', r''),
         (r'</p>', r'\n\n'),
         (r'</(ol|ul)>(?!</(li|[ou]l)>)', r'</\1>\n'),
@@ -25,7 +26,7 @@ def main():
         html = ''.join(filter(None, markdown(f.read()).split('\n')))
 
     for a, b in translate:
-        html = sub(a, b, html, MULTILINE)
+        html = sub(a, b, html)
 
     with open('README.html', 'w', encoding='utf-8') as f:
         f.write(html.strip())
