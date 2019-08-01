1b5a5d2cac4dfeb4c5fe9eb5a71a2447137dcfe1
luoliyan <joseph@lorimer.me>
2019-08-01 22:55:25 +0930

    Add test for frequency lookup

---
+++ tests/test_freq.py
@@ -0,0 +1,27 @@
+# Copyright © 2019 Joseph Lorimer <joseph@lorimer.me>
+#
+# This file is part of Chinese Support Redux.
+#
+# Chinese Support Redux is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Chinese Support Redux is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Chinese Support Redux.  If not, see <https://www.gnu.org/licenses/>.
+
+from chinese.freq import lookup_frequency
+from tests import Base
+
+
+class Frequency(Base):
+    def test_very_basic(self):
+        self.assertEqual(
+            lookup_frequency('上海'),
+            '<div class="freq freq-very-basic">very basic</div>',
+        )
